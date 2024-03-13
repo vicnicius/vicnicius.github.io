194e7338f2f263881aeda9e0d442c97cca5d900e
Vini Andrade <me@vicnicius.com>
2024-03-13 20:02:10 +0100

    Remove Medium posts
    Signed-off-by: Vini Andrade <me@vicnicius.com>

--- getLatestPosts.js
+++ getLatestPosts.js
@@ -4,6 +4,5 @@ const parser = new Parser();
 const pug = require("pug");
 const compiledFunction = pug.compileFile("index.pug");
 (async () => {
-  const feed = await parser.parseURL("http://medium.com/feed/@viniwrites");
-  fs.writeFileSync("index.html", compiledFunction({ posts: feed.items }));
+  fs.writeFileSync("index.html", compiledFunction());
 })();
